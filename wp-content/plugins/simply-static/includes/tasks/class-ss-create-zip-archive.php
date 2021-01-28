11558d68562653aa237b800aab6336da431daf3d
realty-ctc <78147671+realty-ctc@users.noreply.github.com>
2021-01-28 20:09:10 +0800

    delete simply

--- wp-content/plugins/simply-static/includes/tasks/class-ss-create-zip-archive.php
+++
@@ -1,6 +0,0 @@
-<br />
-<b>Warning</b>:  Use of undefined constant ABSPATH - assumed 'ABSPATH' (this will throw an Error in a future version of PHP) in <b>D:\xampp\htdocs\realty-ctccondo\wp-content\plugins\simply-static\includes\tasks\class-ss-create-zip-archive.php</b> on line <b>4</b><br />
-<br />
-<b>Warning</b>:  require_once(ABSPATHwp-admin/includes/class-pclzip.php): failed to open stream: No such file or directory in <b>D:\xampp\htdocs\realty-ctccondo\wp-content\plugins\simply-static\includes\tasks\class-ss-create-zip-archive.php</b> on line <b>4</b><br />
-<br />
-<b>Fatal error</b>:  require_once(): Failed opening required 'ABSPATHwp-admin/includes/class-pclzip.php' (include_path='D:\xampp\php\PEAR') in <b>D:\xampp\htdocs\realty-ctccondo\wp-content\plugins\simply-static\includes\tasks\class-ss-create-zip-archive.php</b> on line <b>4</b><br />
